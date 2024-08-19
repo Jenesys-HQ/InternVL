f41e5edd0b73f5baa958676185f55b900e57711b
manu-laptop <giambi.manuel@gmail.com>
2024-08-19 14:17:51 +0200

    create json2jsonl method

--- internvl_chat/tools/json2jsonl.py
+++ internvl_chat/tools/json2jsonl.py
@@ -2,20 +2,28 @@
 import argparse
 import json
 
-argparse = argparse.ArgumentParser()
-argparse.add_argument('path', type=str)
 
-args = argparse.parse_args()
+def json2jsonl(path: str):
+    data = json.load(open(path))
+    writer = open(path.replace('.json', '.jsonl'), 'w')
+    for idx, item in enumerate(data):
+        conversations = item['conversations']
+        if conversations[0]['from'] == 'system':
+            item['conversations'] = item['conversations'][1:]
+        item['id'] = idx
+        writer.write(json.dumps(item, ensure_ascii=False) + '\n')
 
-assert args.path.endswith('.json')
+    writer.close()
 
-data = json.load(open(args.path))
-writer = open(args.path.replace('.json', '.jsonl'), 'w')
-for idx, item in enumerate(data):
-    conversations = item['conversations']
-    if conversations[0]['from'] == 'system':
-        item['conversations'] = item['conversations'][1:]
-    item['id'] = idx
-    writer.write(json.dumps(item, ensure_ascii=False) + '\n')
 
-writer.close()
+if __name__ == '__main__':
+    argparse = argparse.ArgumentParser()
+    argparse.add_argument('path', type=str)
+
+    args = argparse.parse_args()
+
+    assert args.path.endswith('.json')
+
+    path = args.path
+    json2jsonl(path)
+
