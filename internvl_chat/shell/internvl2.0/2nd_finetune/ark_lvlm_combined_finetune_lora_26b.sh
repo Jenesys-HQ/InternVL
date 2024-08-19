f5ead50b19fa3c560d7c00bf8a59664d90203f0c
manu-laptop <giambi.manuel@gmail.com>
2024-08-19 17:38:35 +0200

    add 26b finetuning script

--- internvl_chat/shell/internvl2.0/2nd_finetune/ark_lvlm_combined_finetune_lora_26b.sh
+++ internvl_chat/shell/internvl2.0/2nd_finetune/ark_lvlm_combined_finetune_lora_26b.sh
@@ -3,7 +3,7 @@ set -x
 CURRENT_DATE=$(date +"%Y-%m-%d-%H-%M-%S")
 GPUS=${GPUS:-1}
 BATCH_SIZE=${BATCH_SIZE:-16}
-PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-4}
+PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-2}
 GRADIENT_ACC=$((BATCH_SIZE / PER_DEVICE_BATCH_SIZE / GPUS))
 
 
@@ -12,15 +12,15 @@ export MASTER_PORT=34229
 export TF_CPP_MIN_LOG_LEVEL=3
 export LAUNCHER=pytorch
 
-OUTPUT_DIR="work_dirs/internvl_chat_v2_0/ark_lvlm_combined_finetune_lora_4b"
+OUTPUT_DIR="work_dirs/internvl_chat_v2_0/ark_lvlm_combined_finetune_lora_26b"
 
 if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir -p "$OUTPUT_DIR"
 fi
 
 # number of gpus: 2
-# batch size per gpu: 4
-# gradient accumulation steps: 2
+# batch size per gpu: 2
+# gradient accumulation steps: 4
 # total batch size: 16
 # epoch: 1
 torchrun \
@@ -30,8 +30,8 @@ torchrun \
   --nproc_per_node=${GPUS} \
   --master_port=${MASTER_PORT} \
   internvl/train/internvl_chat_finetune.py \
-  --model_name_or_path "./pretrained/InternVL2-4B" \
-  --conv_style "phi3-chat" \
+  --model_name_or_path "./pretrained/InternVL2-26B" \
+  --conv_style "internlm2-chat" \
   --output_dir ${OUTPUT_DIR} \
   --meta_path "./shell/data/ark_lvlm_combined_train.json" \
   --overwrite_output_dir True \
@@ -53,7 +53,7 @@ torchrun \
   --save_strategy "steps" \
   --save_steps 200 \
   --save_total_limit 1 \
-  --learning_rate 4e-5 \
+  --learning_rate 2e-5 \
   --weight_decay 0.05 \
   --warmup_ratio 0.03 \
   --lr_scheduler_type "cosine" \
