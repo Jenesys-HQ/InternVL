245a7f4a10224c3ae612d05bf1734a2c1f173772
manu <giambi.manuel@gmail.com>
2024-08-30 11:55:46 +0200

    change RUN_NAME to MLFLOW_RUN_NAME

--- internvl_chat/shell/internvl2.0/eval/ark_lvlm_lora_40b_eval.sh
+++ internvl_chat/shell/internvl2.0/eval/ark_lvlm_lora_40b_eval.sh
@@ -14,7 +14,7 @@ export TF_CPP_MIN_LOG_LEVEL=3
 export LAUNCHER=pytorch
 export MLFLOW_TRACKING_URI="arn:aws:sagemaker:eu-west-1:899757773314:mlflow-tracking-server/test"
 export MLFLOW_EXPERIMENT_NAME="Invoice Extraction"
-export RUN_NAME="${MODEL_NAME}_eval_${CURRENT_DATE}"
+export MLFLOW_RUN_NAME="${MODEL_NAME}_eval_${CURRENT_DATE}"
 
 python tools/eval.py \
   --model-path ${MODEL_PATH} \
