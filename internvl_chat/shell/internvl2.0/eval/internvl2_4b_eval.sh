a545882f312cee8896cd403a5232ceea09495004
manu <giambi.manuel@gmail.com>
2024-08-30 12:01:57 +0200

    use local foundation model

--- internvl_chat/shell/internvl2.0/eval/internvl2_4b_eval.sh
+++ internvl_chat/shell/internvl2.0/eval/internvl2_4b_eval.sh
@@ -1,11 +1,11 @@
 set -x
 
 CURRENT_DATE=$(date +"%Y-%m-%d-%H-%M-%S")
-MODEL_NAME="internvl2_4b"
-OUTPUT_DIR="models/${MODEL_NAME}"
+MODEL_NAME="InternVL2-4B"
+MODEL_PATH="models/${MODEL_NAME}"
 
-if [ ! -d "$OUTPUT_DIR" ]; then
-  mkdir -p "$OUTPUT_DIR"
+if [ ! -d "$MODEL_PATH" ]; then
+  mkdir -p "$MODEL_PATH"
 fi
 
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
@@ -17,6 +17,6 @@ export MLFLOW_EXPERIMENT_NAME="Invoice Extraction"
 export MLFLOW_RUN_NAME="${MODEL_NAME}_eval_${CURRENT_DATE}"
 
 python tools/eval.py \
-  --model-path "pretrained/InternVL2-4B" \
+  --model-path ${MODEL_PATH} \
   --eval-dataset "data/processed_whole/ark-lvlm-combined/test.jsonl" \
-  2>&1 | tee -a "${OUTPUT_DIR}/${CURRENT_DATE}_eval_log.txt"
+  2>&1 | tee -a "${MODEL_PATH}/${CURRENT_DATE}_eval_log.txt"
