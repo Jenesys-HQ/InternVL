86b7e0a6d4ae2adae7284af9af00784e4197f188
manu <giambi.manuel@gmail.com>
2024-09-25 17:34:47 +0800

    change split arg to no-split

--- internvl_chat/internvl/train/historical_learning.py
+++ internvl_chat/internvl/train/historical_learning.py
@@ -4,11 +4,11 @@ from internvl.tools.huggingface import prepare_dataset_for_finetuning
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--split", action="store_true", default=True)
+    parser.add_argument("--no-split", action="store_true", default=False)
     args = parser.parse_args()
 
     dataset_names = ['jenesys-ai/historical_data_9dbed789-a3d4-4d69-95f5-7ff3fcbdbfb8']
     combined_dataset_name = dataset_names[0].split('/')[-1]
     label_column = 'lvlm-historical-annotation.suggestion'
 
-    prepare_dataset_for_finetuning(dataset_names, combined_dataset_name, label_column, split=args.split)
+    prepare_dataset_for_finetuning(dataset_names, combined_dataset_name, label_column, split=~args.no_split)
