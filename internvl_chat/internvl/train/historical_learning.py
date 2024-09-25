32a72832a294f052b100b44563810715d5f1c632
manu <giambi.manuel@gmail.com>
2024-09-25 17:41:58 +0800

    change split arg to no-split

--- internvl_chat/internvl/train/historical_learning.py
+++ internvl_chat/internvl/train/historical_learning.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     combined_dataset_name = dataset_names[0].split('/')[-1]
     label_column = 'lvlm-historical-annotation.suggestion'
 
-    prepare_dataset_for_finetuning(dataset_names, combined_dataset_name, label_column, split=~args.no_split)
+    prepare_dataset_for_finetuning(dataset_names, combined_dataset_name, label_column, split=~bool(args.no_split))
