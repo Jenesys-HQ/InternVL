0ec1d417b8a28556517c0dcf6aa856b6b96f9969
manu <giambi.manuel@gmail.com>
2024-09-26 21:08:30 +0800

    try torchrun for eval script

--- internvl_chat/shell/internvl2.0/eval/internvl2_26b_eval.sh
+++ internvl_chat/shell/internvl2.0/eval/internvl2_26b_eval.sh
@@ -1,6 +1,7 @@
 set -x
 
 CURRENT_DATE=$(date +"%Y-%m-%d-%H-%M-%S")
+GPUS=${GPUS:-1}
 MODEL_NAME="InternVL2-26B"
 MODEL_PATH="models/${MODEL_NAME}"
 
@@ -16,7 +17,13 @@ export MLFLOW_TRACKING_URI="arn:aws:sagemaker:eu-west-1:899757773314:mlflow-trac
 export MLFLOW_EXPERIMENT_NAME="Invoice Extraction"
 export MLFLOW_RUN_NAME="${MODEL_NAME}_eval_${CURRENT_DATE}"
 
-python tools/eval.py \
+torchrun \
+  --nnodes=1 \
+  --node_rank=0 \
+  --master_addr=127.0.0.1 \
+  --nproc_per_node=${GPUS} \
+  --master_port=${MASTER_PORT} \
+  tools/eval.py \
   --model-path ${MODEL_PATH} \
   --eval-dataset "data/processed_whole/ark-lvlm-combined/test.jsonl" \
   2>&1 | tee -a "${MODEL_PATH}/${CURRENT_DATE}_eval_log.txt"
