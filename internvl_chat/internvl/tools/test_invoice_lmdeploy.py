44e239ee29207b9099a21ae81dd596a7f272163d
manu <giambi.manuel@gmail.com>
2024-10-14 14:46:19 +0800

    comment dtype

--- internvl_chat/internvl/tools/test_invoice_lmdeploy.py
+++ internvl_chat/internvl/tools/test_invoice_lmdeploy.py
@@ -209,7 +209,7 @@ def run_main(ckpt_dir: str):
     engine_config = PytorchEngineConfig(
         session_len=8192,
         tp=2,
-        dtype=torch.float16,
+        # dtype=torch.float16,
         device_type="cuda"
     )
     pipe = pipeline(ckpt_dir, backend_config=engine_config)
